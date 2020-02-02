359530f9d226655f136f561b7c784a615366d849
Marc <muilpp@gmail.com>
2020-02-02 19:01:43 +0100

    Added new error message for artists request

--- ArtistRequest.go
+++ ArtistRequest.go
@@ -11,6 +11,11 @@ func getMostListenedArtists(user string, apiKey string, limit string) []Artist {
 	client := http.Client{}
 
 	req, err := http.NewRequest("GET", "http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user="+user+"&api_key="+apiKey+"&format=json&limit="+limit, nil)
+
+	if err != nil {
+		log.Fatal("Error creating request for most listened artists")
+	}
+
 	resp, err := client.Do(req)
 
 	if err != nil {
