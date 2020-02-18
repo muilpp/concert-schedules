871fa0bff9a48ba7a45dd294cbe017ac9288b3e9
Marc <marc.p@xtremis.com>
2020-02-18 15:39:47 +0100

    Added tests for concert utils

---
+++ concertutils/ConcertUtils_test.go
@@ -0,0 +1,58 @@
+package concertutils
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestFindsRepeatedBandAlreadyInSlice(t *testing.T) {
+	var concertSlice []Concert
+
+	concert := CreateConcert("Event", time.Now(), "artist", "venue", "city")
+	concertSlice = append(concertSlice, *concert)
+	repeatedConcert := CreateConcert("Event", time.Now(), "artist", "venue", "city")
+
+	if !IsBandAlreadyInSlice(concertSlice, repeatedConcert.Artist) {
+		t.Errorf("Expected band %v to be in slice, but it's not", concert.Artist)
+	}
+}
+
+func TestDoesNotFindBandInSlice(t *testing.T) {
+	var concertSlice []Concert
+
+	concert := CreateConcert("Event", time.Now(), "artist", "venue", "city")
+	concertSlice = append(concertSlice, *concert)
+	repeatedConcert := CreateConcert("Event", time.Now(), "different artist", "venue", "city")
+
+	if IsBandAlreadyInSlice(concertSlice, repeatedConcert.Artist) {
+		t.Errorf("Expected band %v not to be in slice, but it is", concert.Artist)
+	}
+}
+
+func TestDuplicatedEventsAreRemoved(t *testing.T) {
+	var concertSlice []Concert
+	concertSlice = append(concertSlice, *CreateConcert("Event", time.Now(), "artist1", "venue", "city"))
+	concertSlice = append(concertSlice, *CreateConcert("Event", time.Now(), "artist2", "venue", "city"))
+	concertSlice = append(concertSlice, *CreateConcert("Event", time.Now(), "artist3", "venue", "city"))
+
+	var duplicateConcertSlice []Concert
+	duplicateConcertSlice = append(concertSlice, *CreateConcert("Event", time.Now(), "artist2", "venue", "city"))
+	duplicateConcertSlice = append(concertSlice, *CreateConcert("Event", time.Now(), "artist3", "venue", "city"))
+	duplicateConcertSlice = append(concertSlice, *CreateConcert("Event", time.Now(), "artist4", "venue", "city"))
+
+	cleanConcertSlice := RemoveDuplicateEvents(concertSlice, duplicateConcertSlice)
+
+	totalElementsExpected := 4
+	if len(cleanConcertSlice) != totalElementsExpected {
+		t.Errorf("Duplicates are not removed properly from concert slice, expected %v elements, but %v found", totalElementsExpected, len(cleanConcertSlice))
+	}
+}
+
+func TestSpecialCharactersAreRemovedFromBandName(t *testing.T) {
+	word := RemoveAccents("Bonć diáàñ!")
+
+	if !strings.Contains(word, "Bonc diaan") {
+		t.Errorf("No special characters expected, but %v found", word)
+	}
+}
